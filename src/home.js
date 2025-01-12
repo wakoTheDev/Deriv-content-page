95e5ffc287741c0928db498e463e0a3b8651b96c
wakoTheDev <wakopaul19@gmail.com>
2025-01-12 11:51:38 +0300

    updated the landing page

--- src/home.js
+++ src/home.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "./home.css"; 
 import MyHeader from "./homeheader";
-// import "./newhome.css";
+import BackgroundImage  from "./binaryimage.jpg";
 
 const LandingPage = ({ onStartLearning }) => {
   return (
@@ -13,9 +13,9 @@ const LandingPage = ({ onStartLearning }) => {
 
       {/* Main Content */}
       <main className="content">
-        <h1 className="title">Welcome To BINARY TOOL</h1>
+        <h1 className="title">Welcome To Binary Tool</h1>
         <p className="description">
-          Welcome to the BINARY TOOL, an indispensable asset for traders of digital options and CFDs. This state-of-the-art platform is designed to help you achieve precision in your trades by identifying sniper entries in the market. Utilizing cutting-edge technology, BINARY TOOL provides advanced market analysis, including volatility assessments, pattern sequences, and real-time market signals. Trusted by top options traders globally, this tool offers the insights you need to refine your trading strategies and maximize your potential for success. Always remember to trade responsibly, as the high risks associated with digital options and CFDs can result in significant losses.
+          Welcome to the Binary Tool, an indispensable asset for traders of digital options and CFDs. This state-of-the-art platform is designed to help you achieve precision in your trades by identifying sniper entries in the market. Utilizing cutting-edge technology, Binary Tool provides advanced market analysis, including volatility assessments, pattern sequences, and real-time market signals. Trusted by top options traders globally, this tool offers the insights you need to refine your trading strategies and maximize your potential for success. Always remember to trade responsibly, as the high risks associated with digital options and CFDs can result in significant losses.
         </p>
         <p>
           Take your online and distance learning to new heights. Explore forex, Deriv,
@@ -27,6 +27,10 @@ const LandingPage = ({ onStartLearning }) => {
           <button className="start-learning" onClick={onStartLearning}>
             Start Learning
           </button>
+        </div>
+
+        <div className="image">
+        <img src={BackgroundImage}  alt={""} width={"100%"} height={"100%"} style={{objectFit:"fill"}} />
         </div>
       </main>
     </div>
